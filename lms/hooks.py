1b120d91af60e35cf4cec3642777995dae77b201
Nguyễn Hải Đăng <nhdandz@Nguyens-MacBook-Pro.local>
2026-03-06 00:25:28 +0700

    update homepage

--- lms/hooks.py
+++ lms/hooks.py
@@ -51,7 +51,7 @@ web_include_js = []
 # ----------
 
 # application home page (will override Website Settings)
-# home_page = "login"
+home_page = get_lms_path()
 
 # website user home page (by Role)
 # role_home_page = {
